a1dc3a342e217fc2d255ddd72813929eb8bfe9b5
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2014-01-15 06:07:32 -0500

    orden

--- app/models/caso.rb
+++ app/models/caso.rb
@@ -2,8 +2,8 @@ class Caso < ActiveRecord::Base
     has_many :presponsable, 
       :through => :caso_presponsable
     has_many :caso_presponsable, 
-      foreign_key: :id_caso,
       dependent: :destroy,
+      foreign_key: :id_caso,
       validate: true
     accepts_nested_attributes_for :caso_presponsable, 
       allow_destroy: true,
