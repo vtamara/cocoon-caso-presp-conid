0d0b287a5459eb95dbeb31479838aa00e4b9b525
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2014-01-26 07:05:18 -0500

    Actualiza

--- app/models/presponsable.rb
+++ app/models/presponsable.rb
@@ -1,4 +1,4 @@
 class Presponsable < ActiveRecord::Base
-  has_many :caso_presponsable, foreign_key: "id_presponsable", validate: true
   has_many :caso, through: :caso_presponsable
+  has_many :caso_presponsable, foreign_key: "id_presponsable", validate: true
 end
