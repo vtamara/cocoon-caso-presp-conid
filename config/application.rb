53f4d229d8fbb6641a0f7354b8b715032c298ec4
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2014-06-24 20:26:37 -0500

    static assets

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+if defined?(Bundler)
+	# If you want your assets lazily compiled in production, use this line
+	Bundler.require(:default, :assets, Rails.env)
+end
+
 module S12CasoPresp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
